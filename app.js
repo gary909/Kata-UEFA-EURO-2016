62e1877d01c77dfa5734e7693ef5fc802b60a125
gary909 <garywhite61@hotmail.com>
2023-01-10 12:55:52 +0100

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+function uefaEuro2016(teams, scores) {
+    // your code...
+}
+
+console.log(uefaEuro2016(['Germany', 'Ukraine'], [2, 0])); // "At match Germany - Ukraine, Germany won!";
+console.log(uefaEuro2016(['Belgium', 'Italy'], [0, 2])); // "At match Belgium - Italy, Italy won!";
+console.log(uefaEuro2016(['Portugal', 'Iceland'], [1, 1])); // "At match Portugal - Iceland, teams played draw.";
