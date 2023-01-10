0b10e1dbd42613ba69c1f1e9935388bced293d56
gary909 <garywhite61@hotmail.com>
2023-01-10 13:01:53 +0100

    code working

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 function uefaEuro2016(teams, scores) {
     // your code...
+    if (scores[0] > scores[1]) {
+        return "At match " + teams[0] + " - " + teams[1] + ", " + teams[0] + " won!";
+    } else if (scores[0] < scores[1]) {
+        return "At match " + teams[0] + " - " + teams[1] + ", " + teams[1] + " won!";
+    } else {
+        return "At match " + teams[0] + " - " + teams[1] + ", teams played draw.";
+    }
 }
 
 console.log(uefaEuro2016(['Germany', 'Ukraine'], [2, 0])); // "At match Germany - Ukraine, Germany won!";
